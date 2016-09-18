fde0579dc22344a05432c0e96fe8fd610b1e3d80
liujiahua <18811372835@163.com>
2016-09-18 15:08:59 +0800

    第一次

---
+++ app/Http/Controllers/admin/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class LoginController extends BaseController
+{
+    public function index(){
+    	return view('admin/index');
+    }
+}
